58be5174bcbcf94acd9a7baf599591e04fdfe480
figgles <figgles@unknown>
2011-09-01 18:53:24 +0000

    Reduce memory in non-DM levels by removing monster entities rather than just leaving them as empty (but still active)

--- src/quake/monster.c
+++ src/quake/monster.c
@@ -1,20 +1,89 @@
 /****************************\
  Null monster spawn functions
 \****************************/
-void() monster_tarbaby = {};
-void() monster_army = {};
-void() monster_shambler = {};
-void() monster_demon1 = {};
-void() monster_shalrath = {};
-void() monster_zombie = {};
-void() monster_ogre = {};
-void() monster_dog = {};
-void() monster_enforcer = {};
-void() monster_wizard = {};
-void() monster_knight = {};
-void() monster_hell_knight = {};
-void() monster_boss = {};
-void() monster_fish = {};
-void() path_corner = {};
-void() event_lightning = {};
-void() monster_oldone = {};
+void() monster_tarbaby =
+{
+	remove(self);
+};
+
+void() monster_army =
+{
+	remove(self);
+};
+
+void() monster_shambler =
+{
+	remove(self);
+};
+
+void() monster_demon1 =
+{
+	remove(self);
+};
+
+void() monster_shalrath =
+{
+	remove(self);
+};
+
+void() monster_zombie = 
+{
+	remove(self);
+};
+
+void() monster_ogre =
+{
+	remove(self);
+};
+
+void() monster_dog =
+{
+	remove(self);
+};
+
+void() monster_enforcer =
+{
+	remove(self);
+};
+
+void() monster_wizard =
+{
+	remove(self);
+};
+
+void() monster_knight =
+{
+	remove(self);
+};
+
+void() monster_hell_knight =
+{
+	remove(self);
+};
+
+void() monster_boss =
+{
+	remove(self);
+};
+
+void() monster_fish =
+{
+	remove(self);
+};
+
+void() path_corner =
+{
+	remove(self);
+};
+
+void() event_lightning =
+{
+	remove(self);
+};
+
+void() monster_oldone =
+{
+	remove(self);
+};
+
+
